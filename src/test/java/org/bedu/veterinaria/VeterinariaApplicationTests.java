28a4bf9339e16eeeb7923617068ff8729bb0aae3
ricardo.luna <rlunar.dev@gmail.com>
2023-11-27 18:00:51 -0600

    First Commit

---
+++ src/test/java/org/bedu/veterinaria/VeterinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedu.veterinaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
