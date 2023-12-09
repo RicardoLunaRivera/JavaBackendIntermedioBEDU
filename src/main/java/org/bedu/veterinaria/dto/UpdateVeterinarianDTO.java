2fb071e99915f18afed46de9f613b908ef4658c4
CésarSantiago <0199911@up.edu.mx>
2023-12-09 00:36:00 -0600

    Veterinarian API components

--- src/main/java/org/bedu/veterinaria/dto/UpdateVeterinarianDTO.java
+++ src/main/java/org/bedu/veterinaria/dto/UpdateVeterinarianDTO.java
@@ -1,22 +1,31 @@
 package org.bedu.veterinaria.dto;
 
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
 public class UpdateVeterinarianDTO {
     @Id
     private Long idVeterinarian;
 
-    @NotEmpty(message = "Missing Name")
+    @NotEmpty(message = "Missing name")
     private String name;
 
-    @NotEmpty(message = "Missing LastName")
-    private String lastname;
+    @NotEmpty(message = "Missing Lastname")
+     private String lastname;
 
     @NotEmpty(message = "Missing Schedule")
-    private LocalDateTime schedule;
+    private String schedule;
 
     @NotEmpty(message = "Debe de ingresar un telefono")
     @Pattern(regexp = "^(\\d{3}[-]?){2}\\d{4}$") //formato ###-###-####
     private String phone;
 
+    
     @NotEmpty(message = "Missing Specialty")
-    private String specialty;
+     private String specialty;
+
 }
+
