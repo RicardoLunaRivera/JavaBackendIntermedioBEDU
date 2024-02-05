5e6ccaf0dadad39fb2ce3caa12a78650d2b48c28
Nathalie Gonzalez Mena <anath.gmena@gmail.com>
2024-02-05 16:39:28 -0600

    Last push
    Changes on the code

--- build/generated/sources/annotationProcessor/java/main/org/bedu/veterinaria/mapper/VeterinarianMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/org/bedu/veterinaria/mapper/VeterinarianMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-02-05T16:29:41-0600",
+    date = "2024-02-05T16:35:40-0600",
     comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.4.jar, environment: Java 20.0.2 (Oracle Corporation)"
 )
 @Component
--- src/test/java/org/bedu/veterinaria/repository/PetRepositoryTest.java
+++ src/test/java/org/bedu/veterinaria/repository/PetRepositoryTest.java
@@ -36,16 +36,6 @@ class PetRepositoryTest {
   @Test
   @DisplayName("Filtro por nombre de la mascota")
   void findPetByName(){
-    Owner ownerId1 = new Owner();
-    ownerId1.setIdOwner(1L);
-    Owner ownerId2 = new Owner();
-    ownerId2.setIdOwner(2L);
-
-    Veterinarian vetId1 = new Veterinarian();
-    vetId1.setIdVeterinarian(200L);
-
-    Veterinarian vetId2 = new Veterinarian();
-    vetId2.setIdVeterinarian(210L);
 
     Pet pet1 = new Pet();
     Pet pet2 = new Pet();
@@ -54,15 +44,11 @@ class PetRepositoryTest {
     pet1.setSpecies(Species.PERRO);
     pet1.setRace("Criollo");
     pet1.setBirthDate("2022-08-15");
-    pet1.setOwner(ownerId1);
-    pet1.setVeterinarian(vetId1);
 
     pet2.setName("Katto");
     pet2.setSpecies(Species.GATO);
     pet2.setRace("Criollo");
     pet2.setBirthDate("2021-08-15");
-    pet2.setOwner(ownerId2);
-    pet2.setVeterinarian(vetId2);
 
     manager.persist(pet1);
     manager.persist(pet2);
